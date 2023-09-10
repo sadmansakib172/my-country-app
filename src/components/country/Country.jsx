10da08a673205ed83748c6ba1f1af176a792eaa2
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-09-10 18:35:31 +0600

    visited and going state change done

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './country.css'
 const Country = ({country}) => {
-    const {name, flags, population, area} =country;
+    const {name, flags, population, area, cca3} =country;
+    const [visited, setVisited] = useState(false);
+    const handleVisited = () =>{
+        setVisited(!visited)
+    }
+
     return (
         <div className='country'>
             <h3>Name: {name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
+            <p> <small>Code: {cca3}</small> </p>
+            <button onClick={handleVisited}>{visited ? 'visited' : 'Going'}</button>
+            {visited ? 'I have visited this country' : 'i will visit this country one day InsAllah'}
         </div>
     );
 };
